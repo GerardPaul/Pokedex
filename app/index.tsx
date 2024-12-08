3e06ced698862c671b2723f5f5e9b6a69ffba5ce
Gerard Paul <gerardl@surefirelocal.com>
2024-12-08 23:58:43 +0800

    Test Nativewind is working.

--- app/index.tsx
+++ app/index.tsx
@@ -3,13 +3,8 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      className="flex-1 justify-center items-center bg-slate-600">
+      <Text className="font-bold text-3xl text-red-500">PokeDex</Text>
     </View>
   );
 }
