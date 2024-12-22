177bf9015839b434869e9366d1ec624dcd4ff3d9
Gerard Paul P. Labitad <gerardpaul.labitad19@gmail.com>
2024-12-22 13:33:11 +0800

    Update: added labeltext component for label and navigation testing.

--- components/LabelText.tsx
+++ components/LabelText.tsx
@@ -1,11 +1,17 @@
-import { View, Text } from "react-native";
+import { View, Text, Pressable } from "react-native";
 import { Link } from "expo-router";
-import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import Octicons from "@expo/vector-icons/Octicons";
 
 export function LabelText() {
   return (
-    <Link href="/pokemon" className="p-2 bg-red-500 rounded-full text-white">
-      <FontAwesomeIcon icon={"home"} color="white"></FontAwesomeIcon>
-    </Link>
+    <View className="w-full flex-1 flex-row justify-center items-center">
+      <Link href="/home" asChild>
+        <Octicons name="home" size={24} color="#1E293B" />
+      </Link>
+      <Link href="/" asChild>
+        <Ionicons name="caret-back" size={24} color="#1E293B" />
+      </Link>
+    </View>
   );
 }
